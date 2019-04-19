d75deb25673013c531d04495f2763bacdeae5601
Yang Tatehana <yangjh@tsinghua.edu.cn>
2019-04-19 14:52:02 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="keywords" content="online world history map, DIY,世界历史地图,在线自制历史地图,互动地图,在线历史地图DIY">
+<meta name="description" content="在线自制世界上不同王朝各个历史时期的疆域版图">
+<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+
+<title>在线世界历史地图DIY</title> 
+
+</head>
+<body>
+    
+<a href='his/'><img src='ditu.jpg'></a>    
+</br>
+<a href='his/'>在线世界历史地图DIY</a>    
+</br>
+</br>
+</br>
+<b>基本操作</b>
+<ul>
+鼠标拖动，滚轮放大缩小地图，点击空白旋转地图</br>
+*中国和美国可以放大到county（县）级地图</br>
+*地图数据仅供学习参考使用
+</ul>
+</br>
+<b>1、创建年代</b>
+<ul>
+未知年代》输入年份</br>
+</ul>
+</br>
+<b>2、创建王朝</b>
+<ul>
+未知王朝》输入王朝信息</br>
+</ul>
+</br>
+<b>3、设置都城</b>
+<ul>
+放大地图》选择区域》确定都城</br>
+*都城可以设置在state（州）和county（县）级</br>
+</ul>
+</br>
+<b>4、王朝版图</b>
+<ul>
+当前王朝》选择区域》添加该区域》删除该区域</br>
+*当前王朝用红色区域表示</br>
+</ul>
+</br>
+<b>5、修改王朝</b>
+<ul>
+选择当前王朝的首都》修改王朝信息</br>
+*取消都城后可以重新定都</br>
+</ul>
+
+
+</body>
+</html>
+
+</html>
